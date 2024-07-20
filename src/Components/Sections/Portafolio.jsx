2231aa7a73662154a82949013654f3481b90813e
sgonzalez-2022140 <sgonzalez-2022140@kinal.edu.gt>
2024-07-19 22:40:41 -0600

    Agregar a Portafolio (solo el navbar que no hace smoth)

--- src/Components/Sections/Portafolio.jsx
+++ src/Components/Sections/Portafolio.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import { FaJs, FaReact, FaNode } from 'react-icons/fa';
+import { FaJs, FaReact, FaNode, FaJava } from 'react-icons/fa';
 import { SiMongodb } from 'react-icons/si';
 
 const Section = styled.section`
   margin-bottom: 3rem;
   padding-top: 60px;
-  position: relative; /* Añadido para posicionar el modal */
+  position: relative;
 `;
 
 const SectionHeading = styled.h3`
@@ -26,6 +26,7 @@ const CardContainer = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 2rem;
+  flex-wrap: wrap;
 `;
 
 const Card = styled.div`
@@ -33,7 +34,7 @@ const Card = styled.div`
   border-radius: 10px;
   overflow: hidden;
   width: 300px;
-  margin: 0 1rem;
+  margin: 1rem;
   cursor: pointer;
   transition: transform 0.2s;
   &:hover {
@@ -63,7 +64,7 @@ const CardDescription = styled.p`
 
 const Modal = styled.div`
   display: ${(props) => (props.isOpen ? 'flex' : 'none')};
-  position: absolute; /* Cambiado de fixed a absolute */
+  position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
@@ -115,40 +116,81 @@ const IconContainer = styled.div`
 
 const Portfolio = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedProject, setSelectedProject] = useState(null);
 
-  const openModal = () => setIsModalOpen(true);
-  const closeModal = () => setIsModalOpen(false);
+  const projects = [
+    {
+      title: "Gestion de datos",
+      description: "Sistema de camaras en Java con swings",
+      image: "/4to.jpeg"
+    },
+    {
+      title: "Tony´s Kinal",
+      description: "Sistema de camaras en Java con JavaFX",
+      image: "/5to.jpeg"
+    },
+    {
+      title: "Proyecto Almacenadora",
+      description: "Sistema hecho en stack MERN",
+      image: "/Almacenadora.jpeg"
+    },
+    {
+      title: "ANSAR (En desarrollo)",
+      description: "Sistema hecho en stack MERN",
+      image: "/Ansar.jpg"
+    }
+  ];
+
+  const openModal = (project) => {
+    setSelectedProject(project);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedProject(null);
+  };
 
   return (
     <Section id="portfolio">
       <SectionHeading>Portfolio</SectionHeading>
       <SectionText>Estos son unos proyectos que hice</SectionText>
       <CardContainer>
-        <Card onClick={openModal}>
-          <CardImage src="https://via.placeholder.com/300x200" alt="Project" />
-          <CardContent>
-            <CardTitle>Project Title</CardTitle>
-            <CardDescription>Short project description goes here.</CardDescription>
-          </CardContent>
-        </Card>
+        {projects.map((project, index) => (
+          <Card key={index} onClick={() => openModal(project)}>
+            <CardImage src={project.image} alt={project.title} />
+            <CardContent>
+              <CardTitle>{project.title}</CardTitle>
+              <CardDescription>{project.description}</CardDescription>
+            </CardContent>
+          </Card>
+        ))}
       </CardContainer>
-      <Modal isOpen={isModalOpen}>
-        <ModalContent>
-          <ModalHeader>
-            <ModalTitle>Project Title</ModalTitle>
-            <CloseButton onClick={closeModal}>&times;</CloseButton>
-          </ModalHeader>
-          <ModalBody>
-            <p>Here you can explain the project in more detail. Mention the technologies used:</p>
-            <IconContainer>
-              <FaJs size={30} />
-              <FaReact size={30} />
-              <FaNode size={30} />
-              <SiMongodb size={30} />
-            </IconContainer>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+      {selectedProject && (
+        <Modal isOpen={isModalOpen}>
+          <ModalContent>
+            <ModalHeader>
+              <ModalTitle>{selectedProject.title}</ModalTitle>
+              <CloseButton onClick={closeModal}>&times;</CloseButton>
+            </ModalHeader>
+            <ModalBody>
+              <p>Tecnologias que se usaron:</p>
+              <IconContainer>
+                {selectedProject.title === "Gestion de datos" || selectedProject.title === "Tony´s Kinal" ? (
+                  <FaJava size={30} />
+                ) : (
+                  <>
+                    <FaJs size={30} />
+                    <FaReact size={30} />
+                    <FaNode size={30} />
+                    <SiMongodb size={30} />
+                  </>
+                )}
+              </IconContainer>
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      )}
     </Section>
   );
 };
